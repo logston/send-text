486218f2b3c0a588b8fe16f64abd83806a1b2b8b
Paul Logston <code@logston.me>
2016-11-25 18:03:30 -0500

    Refactor scripts

--- run.py
+++ run.py
@@ -6,15 +6,18 @@ import datetime
 import glob
 import os
 import random
+import sys
 
-from send import send_mms
+from send import send_message
 
 DIRNAME = os.path.dirname(os.path.abspath(__file__))
 RELATIVE_NUMBERS_DIR = 'numbers'
 PROGRESS_FILENAME = 'progress.csv'
 
-# Headers
-# Time Sent, File Path
+SMTP_USERNAME = os.environ.get('SMTP_USERNAME')
+SMTP_PASSWORD = os.environ.get('SMTP_PASSWORD')
+SMTP_ADDRESS = os.environ.get('SMTP_ADDRESS')
+SMTP_PORT = int(os.environ.get('SMTP_PORT'))
 
 
 def get_next_file_path_for_number(number_dir):
@@ -30,6 +33,7 @@ def get_next_file_path_for_number(number_dir):
         reader = csv.reader(fp)
         rows = list(reader)
 
+    # Headers: Time Sent, File Path
     files_sent = set(os.path.basename(row[1]) for row in rows)
 
     files_cached = []
@@ -51,16 +55,19 @@ def get_next_file_path_for_number(number_dir):
     return unsent_file
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('number')
-    args = parser.parse_args()
-
-    number_dir = os.path.join(DIRNAME, RELATIVE_NUMBERS_DIR, args.number)
+def run(smtp_address,
+        smtp_port,
+        smtp_username,
+        smtp_password,
+        number,
+        carrier,
+        sender):
+ 
+    number_dir = os.path.join(DIRNAME, RELATIVE_NUMBERS_DIR, number)
 
     file_path = get_next_file_path_for_number(number_dir)
 
-    message = ''
+    message = None
     attachments = ()
     if file_path.endswith('txt'):
         with open(file_path) as fp:
@@ -68,11 +75,15 @@ def main():
     else:
         attachments = (file_path,)
 
-    send_mms(
-        args.number,
-        carrier='t_mobile',
-        sender='noreply@logston.me',
-        subject=None,  # message in text
+    send_message(
+        smtp_address,
+        smtp_port,
+        smtp_username,
+        smtp_password,
+        number,
+        carrier,
+        sender,
+        subject,
         message=message,
         attachments=attachments,
     )
@@ -82,6 +93,50 @@ def main():
         writer = csv.writer(fp)
         now = str(datetime.datetime.utcnow())
         writer.writerow([now, file_path])
+
+
+def main():
+    parser = argparse.ArgumentParser('Send a text')
+    parser.add_argument('-n', '--number')
+    parser.add_argument('-c', '--carrier')
+    parser.add_argument('-s', '--sender')
+    parser.add_argument('-a', '--smtp-address')
+    parser.add_argument('-p', '--smtp-port')
+    parser.add_argument('-U', '--smtp-username')
+    parser.add_argument('-P', '--smtp-password')
+    args = parser.parse_args()
+
+    smtp_address = args.smtp_address or os.environ.get('SMTP_ADDRESS')
+    smtp_port = args.smtp_port or os.environ.get('SMTP_PORT')
+    smtp_username = args.smtp_username or os.environ.get('SMTP_USERNAME')
+    smtp_password = args.smtp_password or os.environ.get('SMTP_PASSWORD')
+    number = args.number or os.environ.get('NUMBER')
+    carrier = args.carrier or os.environ.get('CARRIER')
+    sender = args.sender or os.environ.get('SENDER')
+
+    required_args = ('smtp_address', 'smtp_port',
+                     'smtp_username', 'smtp_password',
+                     'number', 'carrier', 'sender')
+
+    exit = False
+    for arg in required_args:
+        if not locals().get(arg):
+            exit = True
+            print('{} not defined'.format(arg))
+
+        if exit: 
+            parser.print_help()
+            sys.exit(1)
+
+    run(
+        smtp_address=smtp_address,
+        smtp_port=smtp_port,
+        smtp_username=smtp_username,
+        smtp_password=smtp_password,
+        number=number,
+        carrier=carrier,
+        sender=sender,
+    )
 
 
 if __name__ == '__main__':
--- send.py
+++ send.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-'''
-This program texts a user when run.
-'''
-import argparse
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -12,86 +7,99 @@ import smtplib
 
 
 DIRNAME = os.path.dirname(os.path.abspath(__file__))
-USER_NAME = os.environ.get('USER_NAME')
-PASSWORD = os.environ.get('PASSWORD')
-SMTP_ADDRESS = os.environ.get('SMTP_ADDRESS')
-SMTP_PORT = int(os.environ.get('SMTP_PORT'))
 
 
 def get_us_mms_gateways(carrier):
     with open(os.path.join(DIRNAME, 'gateways.json')) as fp:
         gateways = json.load(fp)
 
-    return set(gateways['mms_carriers']['us'][carrier][1:])
+    return gateways['mms_carriers']['us'][carrier][1]
 
 
-def send_email(recipients=(), sender='', subject='', message='', attachments=()):
-    msg = MIMEMultipart() if attachments else MIMEMultipart('alternative')
+def get_us_sms_gateways(carrier):
+    with open(os.path.join(DIRNAME, 'gateways.json')) as fp:
+        gateways = json.load(fp)
+
+    return gateways['sms_carriers']['us'][carrier][1]
+
+
+def build_sms(number,
+              carrier,
+              sender,
+              subject,
+              message):
+
+    gateway = get_us_sms_gateways(carrier)
+    recipient = gateway.format(number=number)
+
+    msg = MIMEMultipart('alternative')
     msg['Subject'] = subject
     msg['From'] = sender
-    msg['To'] = ', '.join(recipients)
+    msg['To'] = recipient
 
-    if message:
-        # Add two spaces to end of message because it seems to get truncated.
-        # I don't know why.
-        message += '  '
-        msg.attach(MIMEText(message))
+    # Add two spaces to end of message because it seems to get truncated.
+    # I don't know why.
+    msg.attach(MIMEText(message + '  '))
+
+    return msg
+
+
+def build_mms(number,
+              carrier,
+              sender,
+              subject,
+              attachments=()):
+
+    gateway = get_us_mms_gateways(carrier)
+    recipient = gateway.format(number=number)
+
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = recipient
+
+    for attachment in attachments:
+        with open(attachment, 'rb') as fp:
+            img = MIMEImage(fp.read())
+        msg.attach(img)
+
+    return msg
+
+
+def send_message(smtp_address,
+                 smtp_port,
+                 smtp_username,
+                 smtp_password,
+                 number,
+                 carrier,
+                 sender,
+                 subject,
+                 message=None,
+                 attachments=()):
+
+    smtp_server = smtplib.SMTP_SSL(smtp_address, smtp_port)
+    smtp_server.ehlo()
+    smtp_server.login(smtp_username, smtp_password)
 
     if attachments:
-        for attachment in attachments:
-            with open(attachment, 'rb') as fp:
-                img = MIMEImage(fp.read())
-            msg.attach(img)
-
-    # Send the email via our own SMTP server.
-    s = smtplib.SMTP_SSL(SMTP_ADDRESS, SMTP_PORT)
-    s.ehlo()
-    s.login(USER_NAME, PASSWORD)
-    s.send_message(msg)
-    s.quit()
-
-
-def send_mms(number,
-             carrier,
-             sender,
-             subject,
-             message='',
-             attachments=()):
-    gateways = get_us_mms_gateways(carrier)
-
-    recipients = set()
-    for gateway in gateways:
-        recipients.add(gateway.format(number=number))
-
-    recipients = sorted(recipients)
-
-    for recipient in recipients:
-        send_email(
-            (recipient,),
+        msg = build_mms(
+            number=number,
+            carrier=carrier,
+            smtp_server=smtp_server,
+            sender=sender,
+            subject=subject,
+            attachments=attachments,
+        )
+    else:
+        msg = build_sms(
+            number=number,
+            carrier=carrier,
+            smtp_server=smtp_server,
             sender=sender,
             subject=subject,
             message=message,
-            attachments=attachments,
         )
 
-
-def main():
-    parser = argparse.ArgumentParser('Send text via email')
-    parser.add_argument('number')
-    args = parser.parse_args()
-
-    gateways = get_us_mms_gateways()
-    recipients = sorted(gateway.format(number=args.number) for gateway in gateways)
-
-    send_email(
-        recipients,
-        sender='noreply@logston.me',
-        subject='test subject 3',
-        message='test message 3'
-        #attachments=('/users/paul/pictures/pyvideo.png',),
-    )
-
-
-if __name__ == '__main__':
-    main()
+    send_server.send_message(msg)
+    send_server.quit()
 
