70e327484380620e7a43888b0dc4804dbfd28fd8
Paul Logston <code@logston.me>
2016-11-25 23:02:28 -0500

    Add SendLog model

--- send/models.py
+++ send/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class SendLog(models.Model):
+    email = models.EmailField()
+
+    sender = models.EmailField()
+
+    subject = models.CharField(max_length=254)
+
+    message = models.TextField(
+        max_length=254,
+        null=True,
+        blank=True,
+    )
+
+    attachment = models.URLField(
+        null=True,
+        blank=True
+    )
+
